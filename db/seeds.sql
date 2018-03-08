1acfac6e480bead23c771b060e24847fc8cb23b3
anishbnair <nbanish@gmail.com>
2018-03-08 16:10:34 -0500

    created values for the database table

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Fish Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES (' Vegan Burger', true);
+
