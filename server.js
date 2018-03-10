8d6166751ddb2d355c352014f6d9bc431a9b58c1
anishbnair <nbanish@gmail.com>
2018-03-09 20:14:09 -0500

    updated server.js file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Require these npm packages
 var express = require("express");
 var bodyParser = require("body-parser");
-// var methodOverride = require("method-override");
+var methodOverride = require("method-override");
 
 var port = process.env.PORT || 3000;
 
